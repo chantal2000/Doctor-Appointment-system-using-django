d0bfd4324d5d912791869a3732451b1541092eb6
chantal2000 <nniyonkuruchantal@gmail.com>
2021-08-09 17:10:50 +0300

    Doctor appointment

--- bookingapp/views.py
+++ bookingapp/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,Group
 from .models import *
 from django.contrib.auth import authenticate,logout,login
-from django.utils import timezone
 
 def homepage(request):
     return render(request,'index.htm')
