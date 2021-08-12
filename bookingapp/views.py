308e081ce4e0ceb85775ac6504b2de8a283c4083
chantal2000 <nniyonkuruchantal@gmail.com>
2021-08-12 22:42:41 +0300

    doctor booking system

--- bookingapp/views.py
+++ bookingapp/views.py
@@ -1,7 +1,6 @@
 from django.core.checks import messages
 from django.shortcuts import render
 
-# Create your views here.
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,Group
 from .models import *
